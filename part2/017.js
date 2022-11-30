50d53b3c88f05ecca442a2f0680400fe1328853b
kangbohyeon <qhgus535@naver.com>
2022-11-30 17:50:45 +0900

    2022.11.30 study

---
+++ part2/017.js
@@ -0,0 +1,4 @@
+console.log("I'am in jeju");
+console.log('Sewha ocean is wonderful');
+console.log(`Have you ever eaten Makgolli?`);
+console.log("this is the firest line\nand this is the second");
