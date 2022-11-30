0ef62e52ea82e37d520f42d39f365c79d5ced39b
kangbohyeon <qhgus535@naver.com>
2022-11-30 11:08:01 +0900

    first commit

---
+++ part1/008.js
@@ -0,0 +1,17 @@
+var x = 5;
+var y = 'five';
+var isTrue = true;
+var empty = null;
+var nothing
+var sym = Symbol('me');
+
+var item = {
+    price: 5000,
+    count: 10
+};
+var fruits = ['apple', 'orange', 'kiwi'];
+var addFruit = function (fruit) {
+    fruits.push(fruit);
+}
+addFruit('watermelon');
+console.log(fruits);
