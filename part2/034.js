e8473a421815a4c9bfa9a06ff405e540340c2e07
kangbohyeon <qhgus535@naver.com>
2022-12-14 22:00:53 +0900

    2022.12.14 study

---
+++ part2/034.js
@@ -0,0 +1,10 @@
+var greetin_expression = function (name) {
+    console.log('Hi :' + name);
+}
+
+function greetin_declaration(name) {
+    console.log('Hi :' + name);
+}
+
+greetin_expression('Chloe');
+greetin_declaration('Chloe');
