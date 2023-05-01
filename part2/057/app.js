246227818e575230e20abfdc92284fa8bb785347
kangbohyeon <qhgus535@naver.com>
2023-05-01 21:51:36 +0900

    2023.5.1 study

---
+++ part2/057/app.js
@@ -0,0 +1,4 @@
+import Hello from "./057.js";
+
+const abc = new Hello("안녕하세요");
+abc.hi("하린");
---
+++ part2/058/app.js
@@ -0,0 +1,8 @@
+import { add, Persion, version } from "./hello.js";
+
+const result = add(1, 2);
+const harin = new Persion("하린");
+
+console.log(result);
+console.log(harin.name);
+console.log(version);
