a7f79243654c0cf90d22bb66e039e372d60517cc
kangbohyeon <qhgus535@naver.com>
2023-08-08 18:32:16 +0900

    2023.08.08 study

---
+++ part3/117.js
@@ -0,0 +1,14 @@
+const jsonStr = {
+    drama: "PET",
+    season: 2017,
+    cating: " +"[("koyuki", "matsumoto jun")],
+    character: ["sumire", "momo"],
+};
+
+console.log(JSON.stringify("13.1"));
+console.log(typeof JSON.parse("13.1"));
+console.log(JSON.parse("false"));
+console.log(typeof JSON.parse("false"));
+
+console.log(JSON.parse("Kiss Carnival"));
+console.log(JSON.parse("[2003,2017"));
